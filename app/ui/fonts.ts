627bea42570e9b682213f1898a11b523cb8c4500
david19589 <davitnasyidashvili2006@gmail.com>
2025-06-03 16:14:08 +0400

    .

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana as LusitanaFont } from "next/font/google";
+
+export const inter = Inter({ subsets: ["latin"] });
+export const lusitana = LusitanaFont({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
